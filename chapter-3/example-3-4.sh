06452adb2248399986a30e6658766fa31909eb1e
dingchangjiang <dcj227@126.com>
2015-08-16 20:05:32 +0800

    example-3-4.sh.

---
+++ chapter-3/example-3-4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 在一个"tarball"中(经过tar和gzip处理理过的文件)
+#+ 备份最后24小时当前目录下d所有修改到文件
+
+BACKUPFILE=backup-$(date +%m-%d-%Y)
+#					在备份文件中嵌入时间.
+#					Thanks, Joshua Tschida, for the idea.
+archive=${1:-$BACKUPFILE}
+# 如果在命令中没有指定备份文件的文件名，
+#+ 那么将默认使用"backup-MM-DD-YYYY.tar.gz".
+
+
